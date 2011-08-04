eaed1b85134682aaedb17fea5a0bfd28d9142793
Christoph <cheichler@uos.de>
2011-08-04 15:06:31 +0200

    Rating Validatierungen hinzugefügt

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -18,13 +18,25 @@ class Rating < ActiveRecord::Base
   validates_presence_of :trip_id, :receiver_id, :author_id
   validates_numericality_of :mark, :only_interger => true, :message => "Note kann nur ganze Zahl sein"
   validates_inclusion_of :mark, :in => 1..6, :message => "Note kann nur von 1 bis 6 verteilt werden"
-  validate :no_double_rating, :authenticate_rater
+  validate :no_double_rating, :authenticate_rater, :no_future_rating, :no_self_rating
 
   def no_double_rating
     self.trip.ratings.each do |r|
       if r.author == self.author and r.receiver == self.receiver
         errors.add(:field, 'Man kann einen User nicht zweimal für einen Trip bewerten!')
       end
+    end
+  end
+  
+  def no_future_rating
+    if self.trip.start_time > Time.now
+      errors.add(:field, "Du bist noch nicht gefahren!")
+    end
+  end
+  
+  def no_self_rating
+    if self.author == self.receiver
+      errors.add(:fields, "Man kann sich nicht selbst bewerten!")
     end
   end
   
