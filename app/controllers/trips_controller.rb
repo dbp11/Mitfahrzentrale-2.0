82df8ba816f40e13dd6f021b9d4df5a00f7206ad
Henning <hstruebe@uos.de>
2011-08-04 14:50:55 +0200

    trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -27,15 +27,9 @@ class TripsController < ApplicationController
     @MITFAHRER = 1
     @POTENTIELLER_MITFAHRER = 2
     @GAST = 3
-
     @user = current_user
-    
     @trip = Trip.find(params[:id])
 
-    @commited_passenger = @trip.get_committed_passengers
-    @uncommited_passenger = @trip.get_uncommitted_passengers
-    @free_seats = @trip.get_free_seats
-    @occupied_seats = @trip.get_occupied_seats
     if current_user == @trip.user
       flash[:notice] = "FAHRER"
       @status = @FAHRER
@@ -49,18 +43,42 @@ class TripsController < ApplicationController
       flash[:notice] = "GAST"
       @status = @GAST
     end
+    @free_seats = @trip.get_free_seats
+    @occupied_seats = @trip.get_occupied_seats
 
     if params[:request]
-      current_user.bewerben(@trip)
-      #geht noch nicht 
+      if @free_seats - @occupied_seats > 0
+        current_user.bewerben(@trip)
+      end
     end
 
     if params[:accept] and @status == @FAHRER
-      #Methdoe
+      temp = User.find(params[:uid])
+      @trip.accept(temp)
     end
     
-    if params[:decline] and @status == @FAHRER
-      #Methode
+    if params[:decline] and @status != @GAST
+      temp = User.find(params[:uid])
+      @trip.declined(temp)
+    end
+
+    @commited_passenger = @trip.get_committed_passengers
+    @uncommited_passenger = @trip.get_uncommitted_passengers
+    @free_seats = @trip.get_free_seats
+    @occupied_seats = @trip.get_occupied_seats
+
+    if current_user == @trip.user
+      flash[:notice] = "FAHRER"
+      @status = @FAHRER
+    elsif @trip.user_committed (current_user)
+      flash[:notice] = "MITFAHRER"
+      @status = @MITFAHRER
+    elsif @trip.user_uncommitted (current_user)
+      flash[:notice] = "POTENTIELLER_MITFAHRER"
+      @status = @POTENTIELLER_MITFAHRER
+    else
+      flash[:notice] = "GAST"
+      @status = @GAST
     end
 
     respond_to do |format|
