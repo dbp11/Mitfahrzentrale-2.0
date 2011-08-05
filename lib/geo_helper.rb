869377a7447ed015e2e04a1f7fc0404a69aab99a
Dominik Krümpelmann <dkruempe@uos.de>
2011-08-05 14:13:18 +0200

    GeoHelper

--- lib/geo_helper.rb
+++ lib/geo_helper.rb
@@ -9,15 +9,23 @@ class GeoHelper
   
   #Berechnet die Route 
   def self.destination(start_end, options = {}, output = "pretty") 
+    puts "GeoHelper#destination"
     if api == :gmaps
       Gmaps4Rails.destination(start_end, options, output)      
-    else if api == :geocoder
-      Geocoder::Calculations.distance_between(start_end['from'],start_end['to'],{:units => :km})
+    elsif api == :geocoder
+      puts start_end[:from]
+      puts start_end[:to]
+      Geocoder::Calculations.distance_between(start_end[:from],start_end[:to],{:units => :km})
     end
   end
   
 
   #Liefert für Koordinaten die Standorte
-  def self.geocode
+  def self.geocode(cord_at_N, cord_at_E)
+    if api == :gmaps
+      Gmaps4rails.geocode(cord_at_N.to_s+ "N "+ cord_at_E+"E","de")[0][:full_data]
+    elsif api == :geocoder
+      Geocoder.address([cord_at_N,cord_at_E])
+    end
   end
 end
