83d7eaf0fa9fea51edf15b869448ae720aeb0de2
Dominik Krümpelmann <dkruempe@uos.de>
2011-08-05 14:53:28 +0200

    User last_raitngs

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,8 +50,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation, 
     :remember_me, :address, :zipcode, :birthday, :city, :sex, :phone, 
     :instantmessenger, :visible_age, :visible_address, :visible_zip, 
-    :visible_phone, :visible_city, :visible_im, :visible_email, :visible_cars, :last_delivery, :user_type, :email_notifications, :business
- 
+    :visible_phone, :visible_city, :visible_im, :visible_email, :visible_cars, :last_delivery, :user_type, :email_notifications, :business, :last_ratings 
   #Von Paperclip gefordertes Statement zum Anhängen von Bildern
   has_attached_file :pic, :styles => { :medium =>  "300x300>", 
                                            :thumb => "100x100>"}
