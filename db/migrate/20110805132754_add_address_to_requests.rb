a434f110b719b44397c7d955e35f0e4222c7d84e
Christoph <cheichler@uos.de>
2011-08-05 15:29:54 +0200

    Trip und Requestmigration

---
+++ db/migrate/20110805132754_add_address_to_requests.rb
@@ -0,0 +1,22 @@
+class AddAddressToTrip < ActiveRecord::Migration
+  def up
+    remove_column :requests, :address_start, :string
+    remove_column :requests, :address_end, :string
+    add_column :requests, :start_zipcode, :integer
+    add_column :requests, :start_street, :string
+    add_column :requests, :start_city, :string
+    add_column :requests, :end_zipcode, :integer
+    add_column :requests, :end_street, :string
+    add_column :requests, :end_city, :string
+  end
+  def down
+    add_column :requests, :address_start, :string
+    add_column :requests, :address_end, :string
+    remove_column :requests, :start_zipcode, :integer
+    remove_column :requests, :start_street, :string
+    remove_column :requests, :start_city, :string
+    remove_column :requests, :end_zipcode, :integer
+    remove_column :requests, :end_street, :string
+    remove_column :requests, :end_city, :string
+  end
+end
