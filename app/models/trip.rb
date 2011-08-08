aa271bff5f8d65c72e4748a651597d9c9c1564ac
mibloemer <michael_bloemer@web>
2011-08-08 17:27:38 +0200

    Methode für Trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -83,10 +83,11 @@ class Trip < ActiveRecord::Base
   has_many :passengers, :dependent => :destroy
   
 
-  before_save :set_address_info, :set_route
+ # before_save :set_address_info, :set_route
 
   def set_address_info
     
+    puts "hallo da bin ich"
     start_a =  Gmaps4rails.geocode(self.starts_at_N.to_s  + "N " + 
                self.starts_at_E.to_s + "E", "de")[0][:full_data]
     
@@ -196,12 +197,12 @@ class Trip < ActiveRecord::Base
          [starts_at_N, starts_at_E], :units => :km) <= t.start_radius) and
         # prüfe ob die Entfernung des Endortes in einem bestimmten Radius übereinstimmt
          ((Geocoder::Calculations.distance_between [t.ends_at_N, t.ends_at_E], 
-         [ends_at_N, ends_at_E], :units => :km)  <= t.end_radius) and
+         [ends_at_N, ends_at_E], :units => :km)  <= t.end_radius)) and
         # prüfe ob der User der den Request gestellt hat ignoriert wird
-         !self.user.ignorings.include?(t.user) and (!self.baggage and !t.baggage or self.baggage) and 
-         self.get_free_seats >= 1) then erg << t
-      end
+         !self.user.ignorings.include?(t.user) then
         # Ergebnisarray mit Requests füllen auf denen diese Eigenschaften zutreffen
+        erg << t
+      end
     end
     return erg
   end
@@ -307,7 +308,7 @@ class Trip < ActiveRecord::Base
   #
   # @return Distanz ( x Km)
   def get_route_distance
-    return distance.to_s + " km"
+    return (distance / 1000).round(3).to_s + "km"
   end
 
   # Gibt aus ob ein übergeben User für den Trip akzeptiert wurde
