74893a85cbc6cb04716c7e8cc89f9e84576940db
mibloemer <michael_bloemer@web>
2011-08-08 10:01:43 +0200

    doku

--- app/models/message.rb
+++ app/models/message.rb
@@ -22,21 +22,21 @@ class Message < ActiveRecord::Base
   validate :no_true_true, :delete_writer_nil, :delete_receiver_nil
   validates_presence_of :message, :subject, :writer_id, :receiver_id 
   
-  #no_true_true für Validate
+  # no_true_true für Validate
   def no_true_true
     if (self.delete_writer? and self.delete_receiver?)
       errors.add(:field, 'Nachricht wird gelöscht!')
     end
   end
   
-  #delete_writer darf nicht Null werden
+  # delete_writer darf nicht Null werden
   def delete_writer_nil
     if(self.delete_writer == nil)
       errors.add(:field, 'delete_writer bzw. delete_receiver nil')
     end
   end
 
-  #delete_receiver darf nicht Null werden
+  # delete_receiver darf nicht Null werden
   def delete_receiver_nil 
     if(self.delete_receiver == nil)
       errors.add(:field, 'delete_writer bzw. delete_receiver nil')
