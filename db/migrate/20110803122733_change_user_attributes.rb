f66307a4719cc57433484ac0c3e6aca45a0c5479
sebrockm <sebrockm@uos.de>
2011-08-09 16:25:52 +0200

    ahhh

--- db/migrate/20110803122733_change_user_attributes.rb
+++ db/migrate/20110803122733_change_user_attributes.rb
@@ -4,10 +4,10 @@ class ChangeUserAttributes < ActiveRecord::Migration
     remove_column :users, :picture_content_type
     remove_column :users, :picture_file_size
     remove_column :users, :picture_updated_at
-    add_column :users, :pic_file_name
-    add_column :users, :pic_content_type
-    add_column :users, :pic_file_size
-    add_column :users, :pic_updated_at
+    add_column :users, :pic_file_name, :string
+    add_column :users, :pic_content_type, :string
+    add_column :users, :pic_file_size, :integer
+    add_column :users, :pic_updated_at , :datetime
   end
 
   def down
