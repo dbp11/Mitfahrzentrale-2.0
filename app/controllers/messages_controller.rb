0eb9cd26d106a5606165630b7f96b09d8ae325f4
DBDominik <dstegman@uos.de>
2011-08-10 13:14:56 +0200

    Statusmessages translated

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -93,7 +93,7 @@ class MessagesController < ApplicationController
     @message.delete_writer = false
 
     if @message.save and !@message.receiver.is_ignored(current_user) 
-      redirect_to messages_path, notice: 'Message was successfully created.'
+      redirect_to messages_path, notice: 'Nachricht wurde erfolgreich erstellt.'
     else
       redirect_to messages_path, notice: 'Nachricht konnte nicht gesendet werden'
     end
@@ -117,11 +117,11 @@ class MessagesController < ApplicationController
   	end
     # Redirect je nachdem, "wer" wir sind
     if params[:who] == "writer"
-      redirect_to "/messages/outbox", notice: 'Message was successfully deleted.'
+      redirect_to "/messages/outbox", notice: 'Nachricht wurde erfolgreich entfernt.'
     elsif params[:who] == "receiver"
-      redirect_to messages_url, notice: 'Message was successfully deleted.'
+      redirect_to messages_url, notice: 'Nachricht wurde erfolgreich entfernt.'
     elsif
-      redirect_to messages_url, notice: 'Message could not be deleted.'
+      redirect_to messages_url, notice: 'Nachricht konnte nicht enfernt werden.'
     end
   end
 
