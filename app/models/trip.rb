a12839b2d52b21d882011e7c09e79949ed490439
sebrockm <sebrockm@uos.de>
2011-08-10 14:30:21 +0200

    kleiner bug hoffentlich weg

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -90,7 +90,8 @@ class Trip < ActiveRecord::Base
                self.starts_at_E.to_s + " ", "de")[0][:full_data]
     
 
-    hausNr = ""
+   
+    hausNr = self.start_street = ""
     start_a["address_components"].each do |i|
       if i["types"].include?("postal_code")
         self.start_zipcode = i["long_name"]
